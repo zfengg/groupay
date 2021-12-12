681f7656fde49d45ff293cad9c0882ca6ae610b2
zfengg <42152221+zfengg@users.noreply.github.com>
2021-12-12 21:11:51 +0800

    fix uniqueness of member names

--- compile.jl
+++ compile.jl
@@ -1,8 +1,7 @@
 #!/usr/bin/env julia
-# julia --project=. -e "using Pkg, PackageCompiler; Pkg.instantiate(); create_app("groupay","compiled")"
 using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 using PackageCompiler
-# create_app("PayGroups.jl", "compiled")
+run(`rm -rf compiled`)
 create_app("Groupay", "compiled")
